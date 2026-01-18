ef24ba1156d80c871446f15fff189ee15383ca63
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-01-18 21:51:31 +0200

    save

--- app/components/search-ticket/SearchTicket.tsx
+++ app/components/search-ticket/SearchTicket.tsx
@@ -5,11 +5,13 @@ function SearchTicket(){
         <div className="row">
             <div className="col-3"></div>
             <div className="col-6">
-                <Form>
+                <Form className="text-center">
+                    <h1>LIVE MORE</h1>
+                    <h3 className="mb-5">Find the fun you are looking for...</h3>
                     <InputGroup className="mb-3">                        
                         <Form.Control
-                            placeholder="Recipient's username"
-                            aria-label="Recipient's username"
+                            placeholder="search for any event"
+                            aria-label="event name"
                             aria-describedby="basic-addon2"
                         />
                         <Button variant="outline-secondary" id="button-addon2">
