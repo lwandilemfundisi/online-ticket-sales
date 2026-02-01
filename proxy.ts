836cf833cf01361b4490e3df37b40b450c496f82
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-02 00:06:15 +0200

    save

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(request: NextRequest){
+    const session = request.cookies.get('__Host-spa');
+    if(!session){
+        const loginUri = new URL('/account/login', request.url);
+        loginUri.searchParams.set('returnUrl', request.nextUrl.pathname + request.nextUrl.search);
+        return NextResponse.redirect(loginUri, { status: 303 })
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/cart']
+}
