feb7d82e8b9ff8cbc0e6f234f4878d4364bfd7cb
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-01 18:32:26 +0200

    save

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+function Cart(){
+    return (
+        <h1>I am the cart page</h1>
+    );
+}
+
+export default Cart;
--- app/components/cart-summary/CartSummary.tsx
+++ app/components/cart-summary/CartSummary.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import React, {useContext} from "react";
+import {useContext} from "react";
 import { CartSummaryContext } from "@/contexts/CartSummaryProvider";
+import currencyFormatter from "@/helpers/currenyFormatter";
+import Link from "next/link";
+import styles from "./CartSummary.module.css"
 
 function CartSummary(){
 
     const {cartSummary} = useContext(CartSummaryContext);
  
     return (
-        <div className="input-group d-flex align-items-center justify-content-center">
-            <span className="input-group-text"><i className="fa fa-shopping-cart pe-4"></i>{cartSummary.totItems}</span>
-            <span className="input-group-text">{cartSummary.totCost}</span>
-        </div>
+        <Link href="/cart" className={styles.cartlink}>
+            <div className="input-group d-flex align-items-center justify-content-center">
+                <span className="input-group-text"><i className="fa fa-shopping-cart pe-4"></i>{cartSummary.totItems}</span>
+                <span className="input-group-text">{currencyFormatter.format(cartSummary.totCost)}</span>
+            </div>
+        </Link>
     );
 }
 
