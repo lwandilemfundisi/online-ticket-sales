ba53ec3bd6c9f60e9a5a24198779a7281991cfa1
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-05 21:25:14 +0200

    save

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
-import { createServer } from 'http';
-import { parse } from 'url';
-import next from 'next';
-
-const dev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 3000;
-const app = next({ dev });
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { createServer } = require('http');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const next = require('next');
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
-
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+    handle(req, res);
+  }).listen(process.env.PORT || 3000);
 });
