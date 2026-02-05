785de662fe82eb1aa0f69d8cbeb12b792c6f41aa
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-05 23:13:16 +0200

    save

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,9 @@ module.exports = {
   apps: [
     { 
         name: "online-ticket-sales",
-        script: "node_modules/next/dist/bin/next",
+        script: "node_modules\\next\\dist\\bin\\next",
         args: "start -p 3000",
-        cwd: "C:/inetpub/wwwroot/online-ticket-sales",
+        cwd: "C:\\inetpub\\wwwroot\\online-ticket-sales",
         env: {
             NODE_ENV: "production"
         }
