4a161aea0604296e4fc1017f8dbb31ecb5282662
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-05 20:40:51 +0200

    save

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import { createServer } from 'http';
+import { parse } from 'url';
 import next from 'next';
 
-const app = next({ dev: false });
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    createServer((req, res) => {
-        handle(req, res);
-    }).listen(3000);
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
 });
