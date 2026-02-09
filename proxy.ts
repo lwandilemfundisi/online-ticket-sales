bc3cda63ddb1314f28c00a213219b571a1bf3c51
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-09 02:33:26 +0200

    save

--- proxy.ts
+++ proxy.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function proxy(request: NextRequest){
     const session = request.cookies.get('__Host-spa');
     if(!session){
-        const loginUri = new URL('/account/login', request.url);
+        const loginUri = new URL('bff/account/login', request.url);
         loginUri.searchParams.set('returnUrl', request.nextUrl.pathname + request.nextUrl.search);
         return NextResponse.redirect(loginUri, { status: 303 })
     }
