0b0cf723fc5cb1c522b6f18f0477850d193c269e
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-09 06:48:19 +0200

    save

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 const { createServer } = require('http');
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const next = require('next');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const fs = require('fs');
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
+const httpsOptions = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
+};
+const port = 3002;
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(httpsOptions, (req, res) => {
     handle(req, res);
-  }).listen(process.env.PORT || 3000);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on https://localhost:${port}`);
+  });
 });
