f25b04cdecade4276ba1332a28a9882c73b0365f
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-10 19:03:36 +0200

    save

--- proxy.ts
+++ proxy.ts
@@ -4,14 +4,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
     try {
-        const rsp = await fetch("bff/auth/user?slide=false");
+        const rsp = await fetch("bff/auth/user?slide=false", {credentials: 'include'});
         const claims = await rsp.json();
         console.log(claims);
         if (!(claims && claims.length > 0)) {
             console.log('We are not authenticated, redirecting to login page');
             const loginUri = new URL('bff/auth/login', request.url);
             loginUri.searchParams.set('returnUrl', request.nextUrl.pathname + request.nextUrl.search);
-            return NextResponse.redirect(loginUri, { status: 303 })
+            return NextResponse.redirect(loginUri, { status: 303 });
         }
         return NextResponse.next();
     } catch (error) {
