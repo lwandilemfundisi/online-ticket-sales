e932fe78081816f7e4d5566d9a3e9527685673e2
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-14 20:42:42 +0200

    save

--- proxy.ts
+++ proxy.ts
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -19,23 +20,23 @@ export async function proxy(req: NextRequest) {
         console.info(`Fetch user claims response status: ${rsp.status}`);
         if (rsp.status === 401) {
             console.warn("Unauthorized response received, redirecting to login.");
-            return clearBffCookie(req);
+            return await clearBffCookie(req);
         } else if (!rsp.ok) {
             console.error(`Failed to fetch user claims: ${rsp.statusText}`);
-            return clearBffCookie(req);
+            return await clearBffCookie(req);
         }
         console.info("User claims fetched successfully, parsing response.");
         const userRsp = await rsp.json();
         console.info(`User claims: ${JSON.stringify(userRsp)}`);
         if (!(userRsp && userRsp.claims && userRsp.claims.length > 0)) {
             console.warn("No claims found in response, redirecting to login.");
-            return redirectToLogin(req);
+            return await clearBffCookie(req);
         }
         console.info("User claims are valid, allowing request to proceed.");
         return NextResponse.next();
     } catch (error) {
         console.error("Error fetching user claims:", error);
-        return redirectToLogin(req);
+        return await clearBffCookie(req);
     }
 }
 
@@ -45,12 +46,12 @@ function redirectToLogin(req: NextRequest) {
     return NextResponse.redirect(loginUrl);
 }
 
-function clearBffCookie(req: NextRequest) {
-    const response = redirectToLogin(req);
+async function clearBffCookie(req: NextRequest) {
+    const cks = await cookies();
     for (const [name] of req.cookies) {
-        response.cookies.delete(name);
+        cks.delete(name);
     }
-    return response;
+    return redirectToLogin(req);
 }
 
 // Apply middleware to /cart routes 
