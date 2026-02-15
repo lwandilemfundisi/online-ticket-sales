fed7194af7021c1e14115300b8f979c08590c00a
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-15 05:04:42 +0200

    save

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -2,29 +2,35 @@
 
 import useProfile from "@/hooks/useProfile";
 import { NavDropdown } from "react-bootstrap";
-import {faUser} from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Profile() {
 
     const { login, logout, isAuthenticated, getNameClaim } = useProfile();
 
-    const signInTitle = (<span>
-        <FontAwesomeIcon icon={faUser} className="pe-4" />
-    </span>)
+    const signIntitle = (
+        <span className="border border-primary rounded-circle p-2 bg-light text-primary">
+            <FontAwesomeIcon icon={faUser} className="pe-4" />
+        </span>
+    );
+
+    const signOutTitle = (
+        getNameClaim()
+    );
 
     return (
         <>
             {
                 isAuthenticated ?
                     <div className="input-group d-flex align-items-center justify-content-center">
-                        <NavDropdown title={getNameClaim()} id="basic-nav-dropdown">
+                        <NavDropdown title={signOutTitle} id="basic-nav-dropdown">
                             <NavDropdown.Item onClick={logout}>SIGN OUT</NavDropdown.Item>
                         </NavDropdown>
                     </div>
                     :
                     <div className="input-group d-flex align-items-center justify-content-center">
-                        <NavDropdown title={signInTitle} id="basic-nav-dropdown">
+                        <NavDropdown title={signIntitle} id="basic-nav-dropdown">
                             <NavDropdown.Item onClick={login}>SIGN IN</NavDropdown.Item>
                         </NavDropdown>
                     </div>
