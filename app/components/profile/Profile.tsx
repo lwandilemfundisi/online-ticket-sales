d0c414ab7cb374f716e4d0db2d403b42c6b823fe
Lwandile Mfundisi <Lwandile.Mfundisi@takealot.com>
2026-02-15 04:48:42 +0200

    save

--- app/components/profile/Profile.tsx
+++ app/components/profile/Profile.tsx
@@ -1,23 +1,33 @@
 "use client"
 
 import useProfile from "@/hooks/useProfile";
+import { NavDropdown } from "react-bootstrap";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Profile(){
+function Profile() {
 
-    const {login, logout, isAuthenticated, getNameClaim} = useProfile();
+    const { login, logout, isAuthenticated, getNameClaim } = useProfile();
+
+    const signInTitle = (<span>
+        <FontAwesomeIcon icon={faUser} className="pe-4" />
+    </span>)
 
     return (
         <>
             {
                 isAuthenticated ?
-                <div className="input-group d-flex align-items-center justify-content-center">
-                    <span className="input-group-text"><i className="fa fa-user pe-4"></i>{getNameClaim()}</span>
-                    <span className="input-group-text" onClick={logout}>SIGN OUT</span>
-                </div>
-                :
-                <div className="input-group d-flex align-items-center justify-content-center">
-                    <span className="input-group-text" onClick={login}><i className="fa fa-user pe-4"></i>SIGN IN</span>
-                </div>
+                    <div className="input-group d-flex align-items-center justify-content-center">
+                        <NavDropdown title={getNameClaim()} id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={logout}>SIGN OUT</NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
+                    :
+                    <div className="input-group d-flex align-items-center justify-content-center">
+                        <NavDropdown title={signInTitle} id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={login}>SIGN IN</NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
             }
         </>
     );
